c22f2505a7d16f156a8554dc04468cc0403703a4
Yallamanda Madhulatha <madhulathayallamanda@gmail.com>
2026-03-23 23:10:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import feedparser
+import requests
 
-url = input("Enter podcast RSS URL: ")
+rss_link = input("Enter podcast RSS link: ")
 
-feed = feedparser.parse(url)
+feed = feedparser.parse(rss_link)
 
-print("\nPodcast Title:", feed.feed.get("title", "No title"))
+print("\nPodcast:", feed.feed.get("title"))
 
-print("\nEpisodes:\n")
+print("\nDownloading first 2 episodes...\n")
 
-for entry in feed.entries[:5]:
-    print("-", entry.get("title", "No title"))
+count = 0
+
+for entry in feed.entries:
+    if count == 2:
+        break
+
+    title = entry.get("title", "no_title")
+    
+    # getting audio link
+    if "enclosures" in entry and len(entry.enclosures) > 0:
+        audio_url = entry.enclosures[0].href
+        
+        print("Downloading:", title)
+
+        try:
+            response = requests.get(audio_url)
+            
+            filename = f"episode_{count}.mp3"
+            with open(filename, "wb") as f:
+                f.write(response.content)
+
+            print("Saved as", filename)
+
+        except:
+            print("Failed to download")
+
+    count += 1
